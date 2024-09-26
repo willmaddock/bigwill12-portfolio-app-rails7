9d9b218061118b7666b9bac16a91abbc5417dda0
willmaddocke <maddockw@msudenver.edu>
2024-09-26 17:36:25 -0600

    Got Bootstrap Working

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js"
